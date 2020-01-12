2dbc77b47efca73b19849e6ec354c7cf9653dbd5
Cleoner Pietralonga <cleonerp@gmail.com>
2020-01-11 23:02:31 -0300

    add gates

--- logicqubit/gates.py
+++ logicqubit/gates.py
@@ -44,6 +44,52 @@ class Gates:
         return M
 
     @staticmethod
+    def U2(phi, _lambda):
+        M = Matrix([[1, -exp(I*_lambda)], [exp(I*phi), exp(I*(phi+_lambda))]])
+        return M
+
+    @staticmethod
+    def U3(theta, phi, _lambda):
+        M = Matrix([[cos(theta/2), -exp(I*_lambda)*sin(theta/2)],
+                    [exp(I*phi)*sin(theta/2), exp(I*(phi+_lambda))*cos(theta/2)]])
+        return M
+
+    @staticmethod
+    def RX(theta):
+        M = Matrix([[cos(theta/2), -I*sin(theta/2)],
+                    [-I*sin(theta/2), cos(theta/2)]])
+        return M
+
+    @staticmethod
+    def RY(theta):
+        M = Matrix([[cos(theta/2), -sin(theta/2)],
+                    [sin(theta/2), cos(theta/2)]])
+        return M
+
+    @staticmethod
+    def RZ(phi):
+        M = Matrix([[exp(-I*phi/2), 0], [0, exp(I*phi/2)]])
+        return M
+
+    @staticmethod
+    def SWAP():
+        M = Matrix([[1, 0, 0, 0], [0, 0, 1, 0],
+                    [0, 1, 0, 0], [0, 0, 0, 1]])
+        return M
+
+    @staticmethod
+    def CCNOT(): # Toffoli Gate
+        M = Matrix([[1, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 1, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 1, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 1, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 1, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 1, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 1],
+                    [0, 0, 0, 0, 0, 0, 1, 0]])
+        return M
+
+    @staticmethod
     def P0():
         M = Matrix([[1, 0], [0, 0]])  # |0><0|
         return M
